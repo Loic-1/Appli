d63c327f409e8e51dad8dd27a03cad5c430d2d8c
Loic-1 <loic.vasile130@gmail.com>
2024-08-01 14:06:14 +0200

    en fait le plus marche mais le reste nan

--- traitement.php
+++ traitement.php
@@ -4,7 +4,7 @@ session_start();
 
 if (isset($_POST['submit'])) {
     // $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
-    $name = htmlspecialchars("name");
+    $name = htmlspecialchars("name" ?? '');
     $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
     $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
@@ -23,24 +23,31 @@ if (isset($_POST['submit'])) {
 if (isset($_GET['action'])) {
     switch ($_GET['action']) {
         case "clear":
+            session_destroy();// ou unset($_SESSION['products']);
             header("Location:recap.php");
-            session_destroy();
             break;
         case "delete":
+            // unset($_SESSION['products']['id']);
             header("Location:recap.php");
             break;
         case "up-qtt":
+            $_SESSION['products'][$index]['qtt']++;
             header("Location:recap.php");
-            $_SESSION['products']['qtt'] = 10;
-            break;
+            exit;
         case "down-qtt":
+            if($_SESSION['products']['qtt']>1){
+                $_SESSION['products']['qtt']['id']--;
+            }
+            else{
+                //delete at index
+            }
             header("Location:recap.php");
             break;
     }
 }
 
 // header("Location:index.php");
-echo "<SCRIPT type='text/javascript'>",
-"window.alert('szss');",
-"window.location.href='index.php';",
-"</SCRIPT>";
+echo "<SCRIPT type='text/javascript'>";
+echo "window.location.href='index.php';";
+echo "window.alert('szss');";
+echo "</SCRIPT>";
