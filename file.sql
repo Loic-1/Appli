05413586f1be6d9ac0c66e3ce6cab3d3809068bc
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-09-24 16:15:31 +0200

    continue upload files

---
+++ file.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS `file` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `file`;
+
+CREATE TABLE IF NOT EXISTS `file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
